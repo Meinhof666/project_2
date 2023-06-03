de8c0e8d7aa777120bf19827472d67460a87ff6c
Said <ilas8389@mail.ru>
2023-06-03 23:49:18 +0300

    first

---
+++ script.js
@@ -0,0 +1,18 @@
+let money = prompt("Ваш бюджет на месяц?", ''),
+    time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+let appData = {
+    budget: money,
+    expenses: '',
+    optionalExpenses: {},
+    income: [],
+    timeData: time,
+    savings: false
+};
+
+let a1 = prompt("Введите обязательную статью расходов в этом месяце", ''),
+	a2 = prompt("Во сколько обойдется?", '');
+
+appData.expenses.a1 = a2;
+
+alert(appData.budget / 30);
