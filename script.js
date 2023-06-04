f52e71f780360466af5b2248a5c658cb5249fa80
Said <ilas8389@mail.ru>
2023-06-04 12:20:21 +0300

    third

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-let money = +prompt("Ваш бюджет на месяц?", ''),
+let money, time;
+
+function start() {
+    money = +prompt("Ваш бюджет на месяц?", '');
     time = prompt("Введите дату в формате YYYY-MM-DD", '');
+
+    while(isNaN(money) || money == "" || money == null) {
+        money = +prompt("Ваш бюджет на месяц?", '');
+    };
+};
+start();
 
 let appData = {
     budget: money,
@@ -10,18 +19,23 @@ let appData = {
     savings: false
 };
 
-for (let i = 0; i < 2; i++) {
-    let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
-	    b = prompt("Во сколько обойдется?", '');
-
-    if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
-        console.log("Done!");
-
-        addData.expenses[a] = b;
+function chooseExpenses() {
+    for (let i = 0; i < 1; i++) {
+        let a = prompt("Введите обязательную статью расходов в этом месяце", ''),
+            b = prompt("Во сколько обойдется?", '');
+    
+        if ((typeof(a)) === 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+            console.log("Done!");
+    
+            appData.expenses[a] = b;
+        } else {
+            i = i - 1;
+        };
     };
 };
+chooseExpenses();
 
-appData.moneyPerDay = appData.budget / 30;
+appData.moneyPerDay = (appData.budget / 30).toFixed();
 
 alert("Ежедневный бюджет: " + appData.moneyPerDay);
 
